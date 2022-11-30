f8dca1a5a881c269e254f60b2bb585001774cbda
peifeng <597927969@qq.com>
2022-11-30 20:14:05 +0800

    题目1性能优化

--- solution/src/main/java/com/peter/wang/leetcode/solution/Solution1.java
+++ solution/src/main/java/com/peter/wang/leetcode/solution/Solution1.java
@@ -1,8 +1,17 @@
 package com.peter.wang.leetcode.solution;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 题目1：两数之和
+ * 题目链接：https://leetcode.cn/problems/two-sum/
+ * @author peter_wang
+ * @date 2022/11/26
+ */
 public class Solution1 {
     /**
-     * 题目链接：https://leetcode.cn/problems/two-sum/
+     *
      * 执行用时：55 ms, 在所有 Java 提交中击败了26.13%的用户
      * 内存消耗：41.9 MB, 在所有 Java 提交中击败了14.90%的用户
      *
@@ -21,6 +30,28 @@ public class Solution1 {
                     break OUT;
                 }
             }
+        }
+
+        return result;
+    }
+
+    /**
+     * 执行用时：4 ms, 在所有 Java 提交中击败了57.55%的用户
+     * 内存消耗：42.6 MB, 在所有 Java 提交中击败了5.02%的用户
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum2(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer, Integer> resultMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (resultMap.get(nums[i]) != null) {
+                result[0] = resultMap.get(nums[i]);
+                result[1] = i;
+            }
+            resultMap.put(target - nums[i], i);
         }
 
         return result;
